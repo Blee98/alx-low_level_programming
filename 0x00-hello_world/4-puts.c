c30a02fe77d5b0425eaafb3e95d79bc028477230
boubker <bouker1998lee@gmail.com>
2023-03-16 19:54:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - our fucntion/entry point
+ *
+ * Description: a C program that prints with puts function
+ *
+ * Return: Always 0 integer
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
