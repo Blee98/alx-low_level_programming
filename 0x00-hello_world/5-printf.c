f43861bc8a653aaa135f625246f8d86a5c99d44a
boubker <bouker1998lee@gmail.com>
2023-03-16 19:57:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is our Entrypoint
+ *
+ * Description: A simple C program that use printf function
+ *
+ * Return: Always 0 mean success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
